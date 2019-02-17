e3f3f49cec9d8692e1255916bf8ecc9c6af39bb6
book-boy <47053228+book-boy@users.noreply.github.com>
2019-02-17 22:49:47 +0800

    文字更改为简体中文
    文字更改为简体中文

--- flagrow/upload/resources/templates/audio.blade.php
+++ flagrow/upload/resources/templates/audio.blade.php
@@ -1,5 +1,6 @@
 <figure class="upl-tpl" data-uuid="{@uuid}">
-	<audio controls><source src="{@url}" type="audio/mpeg">你的瀏覽器不支持 HTML5 音頻，請使用 Vivaldi 瀏覽器訪問。</audio>
+	<audio controls><source src="{@url}" type="audio/mpeg">您当前浏览器不支持HTML5音频播放，请更换或升级浏览器访问。
+	</audio>
   <figcaption>
     <span class="upl-title"><i class="far fa-file-audio"></i>{SIMPLETEXT1}</span>
     <span class="upl-size"><i class="fas fa-cloud-download-alt"></i>{SIMPLETEXT2}</span>
